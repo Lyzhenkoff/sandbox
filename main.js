8b8d7e8f302d516e2a6860f13992184f88505066
kirilllyzhenkoff <kirilllyzhenkoff@gmail.com>
2019-11-06 22:54:01 +0300

    summary

---
+++ main.js
@@ -0,0 +1,11 @@
+let txt = document.getElementById('txt');
+
+ new Vue({
+    el: '#app',
+    data: {
+        title: 'Персик в гневе!',
+        img: 'http://vkclub.su/_data/stickers/persik/sticker_vk_persik_036.png',
+        img2: 'https://vk.com/sticker/1-63-512-9'
+    },
+
+});
